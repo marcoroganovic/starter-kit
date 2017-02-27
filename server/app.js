26ddae7125bf239b614a505369543498ed741ab6
Marco Roganovic <marcoroganovic@gmail.com>
2017-02-27 01:42:10 +0100

    add express and open

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path    = require("path");
+const open    = require("open");
+
+const POST = process.env.PORT || 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../src/index.html"));
+});
+
+app.listen(PORT, (err) => {
+  if(err) {
+    console.log(err);
+  } else {
+    open("http://localhost:" + PORT);
+  }
+});
