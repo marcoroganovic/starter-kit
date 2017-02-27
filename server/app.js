67470cfdfdc3f56de5d9e588f8f30ca14a69cd76
Marco Roganovic <marcoroganovic@gmail.com>
2017-02-27 01:45:36 +0100

    add src folder and log to console when server is started

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path    = require("path");
 const open    = require("open");
 
-const POST = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.get("/", (req, res) => {
@@ -13,6 +13,7 @@ app.listen(PORT, (err) => {
   if(err) {
     console.log(err);
   } else {
+    console.log("Server running on port: " + PORT);
     open("http://localhost:" + PORT);
   }
 });
