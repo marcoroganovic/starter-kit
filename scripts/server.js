f70eb0a08011fa0e5c6c38fc1c39e547b00f169a
Marco Roganovic <marcoroganovic@gmail.com>
2017-02-27 10:40:20 +0100

    add npm scripts

--- scripts/server.js
+++ scripts/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path    = require("path");
 const open    = require("open");
+const chalk   = require("chalk");
+
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -11,9 +13,9 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, (err) => {
   if(err) {
-    console.log(err);
+    console.log(chalk.red(err));
   } else {
-    console.log("Server running on port: " + PORT);
+    console.log(chalk.green("Server running on port: " + PORT));
     open("http://localhost:" + PORT);
   }
 });
